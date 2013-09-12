7d29fe396f4ea8bfe71121165507a14697ef1764
Joe <joestrong@gmail.com>
2013-09-13 00:02:07 +0100

    Added new task button

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,25 @@ var TaskCollectionView = Backbone.View.extend({
 		_.bindAll(this, 'render');
 		this.collection = new TaskCollection();
 		this.collection.bind('sync', this.render);
+		this.collection.bind('add', this.render);
 		this.collection.fetch();
+	},
+	events: {
+		'click #btnNew': 'addTask'
 	},
 	render: function(){
 		var that = this;
+		this.$el.html('');
 		_.each(this.collection.models, function(task){
 			that.$el.append('<div class="task"><input type="checkbox" class="chkDone" />' + task.get('title') + '</div>');
 		});
+		this.$el.append('<input type="text" id="txtNew" />')
+		this.$el.append('<button id="btnNew" class="btn">New task</button>');
+	},
+	addTask: function(){
+		var txtNew = this.$el.find('#txtNew');
+		this.collection.add({ title: txtNew.val() });
+		console.log(this.collection.models);
 	}
 });
 
